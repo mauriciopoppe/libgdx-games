46d8270e2ccbed738ca4bc882ca1296e20b6fdce
Mauricio Poppe <me@maurizzzio.com>
2013-01-25 02:03:56 -0400

    added base classes

--- me-maurizzzio-base-desktop/src/me/maurizzzio/base/Main.java
+++ me-maurizzzio-base-desktop/src/me/maurizzzio/base/Main.java
@@ -11,6 +11,6 @@ public class Main {
 		cfg.width = 480;
 		cfg.height = 320;
 		
-		new LwjglApplication(new MyGdxGame(), cfg);
+		new LwjglApplication(new GdxGame(), cfg);
 	}
 }
--- me-maurizzzio-base-html/src/me/maurizzzio/base/client/GwtLauncher.java
+++ me-maurizzzio-base-html/src/me/maurizzzio/base/client/GwtLauncher.java
@@ -1,6 +1,6 @@
 package me.maurizzzio.base.client;
 
-import me.maurizzzio.base.MyGdxGame;
+import me.maurizzzio.base.GdxGame;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.gwt.GwtApplication;
 import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
@@ -14,6 +14,6 @@ public class GwtLauncher extends GwtApplication {
 
 	@Override
 	public ApplicationListener getApplicationListener () {
-		return new MyGdxGame();
+		return new GdxGame();
 	}
 }
